0c02d71d6c6e3df80cff87a26336659dd35f16ed
Shobhit Umrao <shobhitumrao5@gmail.com>
2025-05-07 06:00:15 +0530

    refactor

---
+++ src/customComponents/SearchBar.tsx
@@ -0,0 +1,38 @@
+import { GradientBackgroundButton } from '@/customComponents/GradientBackgroundButton';
+import search_icon from '@/assets/search_icon.png';
+
+export function SearchBar() {
+  return (
+    <div className="w-full flex items-center py-4">
+      {/* Centered Search Bar */}
+      <div className="flex-grow flex justify-center">
+        <div className="w-[60%] max-w-[860px] min-w-[400px]">
+          <div
+            style={{
+              boxShadow: '0px 0px 60px 10px rgba(97, 243, 8, 0.25)',
+            }}
+            className="inset-0 p-[2px] border-transparent rounded-[30px] custom-border"
+          >
+            <div className="w-full flex items-center bg-[#19191d] px-[18px] py-2 border-2 border-transparent p-4 rounded-[28px] pl-11">
+              <img src={search_icon} alt="search_icon" className="h-4 mr-3" />
+              <input
+                type="text"
+                id="search"
+                name="search"
+                placeholder="Search Matches"
+                className="w-full bg-inherit outline-none text-white font-poppins placeholder:font-poppins font-normal text-[16px] leading-[24px] tracking-[0] placeholder:text-[#A7A8AA] placeholder:opacity-100"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Tip Slip Button aligned to right */}
+      <div className="ml-auto">
+        <GradientBackgroundButton className="h-full py-3 w-[164px]">
+          Tip Slip
+        </GradientBackgroundButton>
+      </div>
+    </div>
+  );
+}
--- src/pages/home-page/index.tsx
+++ src/pages/home-page/index.tsx
@@ -1,9 +1,9 @@
 import { SecondTopbarHeaderBreadcrumb } from '@/customComponents/SecondTopbarHeaderBreadcrumb';
 import { TopbarHeader } from '@/customComponents/TopbarHeader';
-import search_icon from '@/assets/search_icon.png';
-import hero_image from '@/assets/hero_image.jpg';
 import { GradientBackgroundButton } from '@/customComponents/GradientBackgroundButton';
 import { PreviousTipsCard } from '@/customComponents/PreviousTipsCard';
+import { HeroImageComponent } from '@/pages/home-page/components/HeroImageComponent';
+import { SearchBar } from '@/customComponents/SearchBar';
 
 export default function HomePage() {
   console.log('HomePage');
@@ -17,97 +17,9 @@ export default function HomePage() {
         {/* overall padding for all both x side */}
         <div className="w-full px-12 flex flex-col items-start">
           {/* search bar  */}
-          <div className="w-full flex items-center py-4">
-            {/* Centered Search Bar */}
-            <div className="flex-grow flex justify-center">
-              <div className="w-[60%] max-w-[860px] min-w-[400px]">
-                <div
-                  style={{
-                    boxShadow: '0px 0px 60px 10px rgba(97, 243, 8, 0.25)',
-                  }}
-                  className="inset-0 p-[2px] border-transparent rounded-[30px] custom-border"
-                >
-                  <div className="w-full flex items-center bg-[#19191d] px-[18px] py-2 border-2 border-transparent p-4 rounded-[28px] pl-11">
-                    <img src={search_icon} alt="search_icon" className="h-4 mr-3" />
-                    <input
-                      type="text"
-                      id="search"
-                      name="search"
-                      placeholder="Search Matches"
-                      className="w-full bg-inherit outline-none text-white font-poppins placeholder:font-poppins font-normal text-[16px] leading-[24px] tracking-[0] placeholder:text-[#A7A8AA] placeholder:opacity-100"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            {/* Tip Slip Button aligned to right */}
-            <div className="ml-auto">
-              <GradientBackgroundButton className="h-full py-3 w-[164px]">
-                Tip Slip
-              </GradientBackgroundButton>
-            </div>
-          </div>
-
+          <SearchBar />
           {/* hero image section */}
-          <div className="w-full">
-            <div
-              style={{
-                backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.25)), url(${hero_image})`,
-              }}
-              className="bg-cover bg-center h-[608px] rounded-[25px] w-full flex items-start justify-center"
-            >
-              <div className="flex flex-row w-full">
-                {/* Left side (placeholder or image) */}
-                <div className="w-1/2"></div>
-
-                {/* Right side content */}
-                <div className="w-1/2 flex flex-col items-start justify-start text-white mt-24 ml-32">
-                  <div className="font-poppins font-light text-[40px] leading-[48px] tracking-[0] text-left">
-                    <p>“Get Smarter. Win Bigger.</p>
-                    <p>Bet Better With Bettingator”</p>
-                  </div>
-                  <div className="mt-10">
-                    <p className="font-roboto_flex font-light text-[20px] leading-[24px] tracking-[0] text-white text-left">
-                      Data-backed tips and expert predictions at your fingertips.
-                    </p>
-                    <p className="font-roboto_flex font-light text-[20px] leading-[24px] tracking-[0] text-white text-left">
-                      Bet smart, win big.
-                    </p>
-                  </div>
-                  {/* the two buttons */}
-                  <div className="flex flex-row items-center mt-12 gap-16">
-                    <GradientBackgroundButton className="px-11">
-                      Today's Tips
-                    </GradientBackgroundButton>
-                    <GradientBackgroundButton className="px-11">AI Tips</GradientBackgroundButton>
-                  </div>
-
-                  {/* quick stats */}
-                  <div className="flex flex-row items-center mt-40 -ml-12 gap-7 text-center text-white font-lexend font-light text-[20px] leading-[24px] tracking-[0] align-middle">
-                    <p className="flex items-center">
-                      <span className="text-[#61F308] font-lexend font-light text-[20px] leading-[24px] tracking-[0] align-middle">
-                        +89%&nbsp;
-                      </span>
-                      ROI this week
-                    </p>
-                    <p className="flex items-center">
-                      <span className="text-[#61F308] font-lexend font-light text-[20px] leading-[24px] tracking-[0] align-middle">
-                        245&nbsp;
-                      </span>
-                      Winning Tips
-                    </p>
-                    <p className="flex items-center">
-                      <span className="text-[#61F308] font-lexend font-light text-[20px] leading-[24px] tracking-[0] align-middle">
-                        78&nbsp;
-                      </span>
-                      Tipsters Active
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <HeroImageComponent />
           {/* previous tips */}
           <div
             style={{
