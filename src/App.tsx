2c0fcee45925c5030cee37de0441f30fe92a004e
ghost <ghost@devmail.com>
2025-07-07 11:45:37 +0530

    Resolve merge conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { RouterProvider } from 'react-router-dom';
 import router from '@/router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useState } from 'react';
-import { Layout } from '@/components/layout';
+// import { Layout } from '@/components/layout';
 import { Toaster } from '@/components/base/sonner';
 
 function App() {
